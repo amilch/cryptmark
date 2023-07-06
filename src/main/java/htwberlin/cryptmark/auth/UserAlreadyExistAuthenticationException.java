c7f2aaa97e40617e9541fee17b520339633e8f2f
Andreas Milch <mail@andreasmilch.cc>
2023-07-06 03:18:44 +0200

    added http error code to UserAlreadyExistAuthenticationException

--- src/main/java/htwberlin/cryptmark/auth/UserAlreadyExistAuthenticationException.java
+++ src/main/java/htwberlin/cryptmark/auth/UserAlreadyExistAuthenticationException.java
@@ -1,7 +1,10 @@
 package htwberlin.cryptmark.auth;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class UserAlreadyExistAuthenticationException extends AuthenticationException {
     public UserAlreadyExistAuthenticationException(final String username) {
         super("User " + username + " already exists");
