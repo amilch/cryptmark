edb28f9103322d978bf132dc8bfd9f4a9b17b6ef
Andreas Milch <mail@andreasmilch.cc>
2023-07-06 03:20:23 +0200

    Added UserNotFoundException

---
+++ src/main/java/htwberlin/cryptmark/user/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package htwberlin.cryptmark.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super("Could not find user");
+    }
+}
